b7d586812bbe7d28832992a5df630115c5bbc11e
Martin Magr <mmagr@redhat.com>
2020-04-29 22:50:02 +0200

    Generalize config
    Took Config from collectd-sensubility and generalized it, so that we can add
    JSONConfig with same API.

--- config/ini.go
+++ config/ini.go
@@ -2,174 +2,69 @@ package config
 
 import (
 	"fmt"
-	"strconv"
 	"strings"
 
 	"github.com/go-ini/ini"
 	"github.com/infrawatch/apputils/logging"
 )
 
-const (
-	DEFAULT_HOSTNAME = "localhost.localdomain"
-	DEFAULT_IP       = "127.0.0.1"
-)
-
-/******************************************************************************/
-type Validator func(string) error
-
-type Parameter struct {
-	Name       string
-	Default    string
-	Validators []Validator
+//INIConfig holds complete configuration data and metadata for configuration saved as INI file.
+type INIConfig struct {
+	WithConfigBase
 }
 
-type Option struct {
-	value string
-}
-
-type Section struct {
-	Options map[string]*Option
-}
-
-type IniConfig struct {
-	log      *logging.Logger
-	metadata map[string][]Parameter
-	Sections map[string]*Section
-}
-
-/******************************************************************************/
-func OptionsValidatorFactory(options []string) Validator {
-	return func(input string) error {
-		for _, option := range options {
-			if input == option {
-				return nil
-			}
-		}
-		return fmt.Errorf("Value (%v) is not one of allowed options: %v", input, options)
+//NewINIConfig creates and initializes new INIConfig object according to given metadata
+func NewINIConfig(metadata map[string][]Parameter, logger *logging.Logger) *INIConfig {
+	return &INIConfig{
+		WithConfigBase: WithConfigBase{log: logger, metadata: metadata, Sections: make(map[string]*Section)},
 	}
-}
-
-func BoolValidatorFactory() Validator {
-	return func(input string) error {
-		_, err := strconv.ParseBool(input)
-		return err
-	}
-}
-
-func IntValidatorFactory() Validator {
-	return func(input string) error {
-		_, err := strconv.Atoi(input)
-		return err
-	}
-}
-
-func MultiIntValidatorFactory(separator string) Validator {
-	return func(input string) error {
-		for _, item := range strings.Split(input, separator) {
-			_, err := strconv.Atoi(item)
-			if err != nil {
-				return err
-			}
-		}
-		return nil
-	}
-}
-
-/********** Value methods ***********/
-func (opt *Option) GetString() string {
-	return opt.value
-}
-
-func (opt *Option) GetBytes() []byte {
-	return []byte(opt.value)
-}
-
-func (opt *Option) GetStrings(separator string) []string {
-	return strings.Split(opt.value, separator)
-}
-
-func (opt *Option) GetInt() int {
-	output, _ := strconv.Atoi(opt.value)
-	return output
-}
-
-func (opt *Option) GetInts(separator string) []int {
-	options := strings.Split(opt.value, separator)
-	output := make([]int, len(options), len(options))
-	for idx, opt := range options {
-		output[idx], _ = strconv.Atoi(opt)
-	}
-	return output
-}
-
-func (opt *Option) GetBool() bool {
-	output, _ := strconv.ParseBool(opt.value)
-	return output
-}
-
-/** Config methods and fungtions **/
-func validate(value string, validators []Validator) error {
-	for _, validator := range validators {
-		//fmt.Printf("%v - %v\n", value, validator(value))
-		err := validator(value)
-		if err != nil {
-			return fmt.Errorf("Invalid value: %v", value)
-		}
-	}
-	return nil
-}
-
-func NewConfig(metadata map[string][]Parameter, logger *logging.Logger) (*IniConfig, error) {
-	var conf IniConfig
-	conf.metadata = metadata
-	conf.log = logger
-	// initialize config with default values
-	conf.Sections = make(map[string]*Section)
-	for sectionName, sectionMetadata := range conf.metadata {
-		sect := Section{}
-		sect.Options = make(map[string]*Option)
-		conf.Sections[sectionName] = &sect
-		for _, param := range sectionMetadata {
-			if err := validate(param.Default, param.Validators); err != nil {
-				return nil, fmt.Errorf("Failed to validate parameter %s. %s", param.Name, err.Error())
-			}
-			opt := Option{param.Default}
-			sect.Options[param.Name] = &opt
-		}
-	}
-	return &conf, nil
 }
 
 //Parse loads data from given file
-func (conf *IniConfig) Parse(path string) error {
-	data, err := ini.LoadSources(ini.LoadOptions{AllowPythonMultilineValues: true}, path)
+func (conf *INIConfig) Parse(path string) error {
+	options := ini.LoadOptions{
+		AllowPythonMultilineValues: true,
+		IgnoreInlineComment:        true,
+	}
+	data, err := ini.LoadSources(options, path)
 	if err != nil {
 		return err
 	}
 	for sectionName, sectionMetadata := range conf.metadata {
+		conf.Sections[sectionName] = &Section{Options: make(map[string]*Option)}
 		if sectionData, err := data.GetSection(sectionName); err == nil {
 			for _, param := range sectionMetadata {
+				var value interface{}
 				if paramData, err := sectionData.GetKey(param.Name); err == nil {
-					if err := validate(paramData.Value(), param.Validators); err != nil {
-						return fmt.Errorf("Failed to validate parameter %s. %s", param.Name, err.Error())
-					}
-					conf.Sections[sectionName].Options[param.Name].value = paramData.Value()
-					conf.log.Metadata(map[string]interface{}{
-						"section": sectionName,
-						"option":  param.Name,
-						"value":   paramData.Value(),
-					})
-					conf.log.Debug("Using parsed configuration value.")
+					value = paramData.Value()
 				} else {
-					conf.log.Metadata(map[string]interface{}{
-						"section": sectionName,
-						"option":  param.Name,
-						"value":   conf.Sections[sectionName].Options[param.Name].value,
-					})
-					conf.log.Debug("Using default configuration value.")
+					value = nil
+				}
+				if opt, err := createOption(value, param, conf.log); err == nil {
+					conf.Sections[sectionName].Options[param.Name] = opt
+				} else {
+					return err
 				}
 			}
 		}
 	}
 	return nil
+}
+
+//GetOption returns Option objects according to given "section-name/option-name" string.
+func (conf *INIConfig) GetOption(name string) (*Option, error) {
+	var option *Option
+	var err error
+
+	nameparts := strings.SplitN(name, "/", 1)
+	if section, ok := conf.Sections[nameparts[0]]; ok {
+		if opt, ok := section.Options[nameparts[1]]; ok {
+			option = opt
+		} else {
+			err = fmt.Errorf("did not find option '%s' in section '%s'", nameparts[1], nameparts[0])
+		}
+	} else {
+		err = fmt.Errorf("did not find section '%s'", nameparts[0])
+	}
+	return option, err
 }
---
+++ config/validators.go
@@ -0,0 +1,103 @@
+package config
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+//StringOptionsValidatorFactory creates validator for checking if the validator's given value is one of the factory's given string options.
+func StringOptionsValidatorFactory(options []string) Validator {
+	return func(input interface{}) (interface{}, error) {
+		if val, ok := input.(string); ok {
+			for _, option := range options {
+				if val == option {
+					return val, nil
+				}
+			}
+		}
+		return nil, fmt.Errorf("value (%v) is not one of allowed options: %v", input, options)
+	}
+}
+
+//BoolValidatorFactory creates validator for checking if the validator's given value is bool.
+func BoolValidatorFactory() Validator {
+	return func(input interface{}) (interface{}, error) {
+		switch reflect.TypeOf(input).Kind() {
+		case reflect.Bool:
+			return reflect.ValueOf(input).Elem(), nil
+		case reflect.String:
+			val, err := strconv.ParseBool(input.(string))
+			if err == nil {
+				return val, nil
+			}
+			fallthrough
+		default:
+			return nil, fmt.Errorf("value (%v) is not bool", input)
+		}
+	}
+}
+
+//IntValidatorFactory creates validator for checking if the validator's given value is int.
+func IntValidatorFactory() Validator {
+	return func(input interface{}) (interface{}, error) {
+		switch reflect.TypeOf(input).Kind() {
+		case reflect.Int:
+			fallthrough
+		case reflect.Int8:
+			fallthrough
+		case reflect.Int16:
+			fallthrough
+		case reflect.Int32:
+			fallthrough
+		case reflect.Int64:
+			return reflect.ValueOf(input).Elem(), nil
+		case reflect.String:
+			val, err := strconv.ParseInt(input.(string), 10, 64)
+			if err == nil {
+				return val, nil
+			}
+			fallthrough
+		default:
+			return nil, fmt.Errorf("value (%v) is not int", input)
+		}
+	}
+}
+
+//MultiIntValidatorFactory creates validator for checking if the validator's given value
+//is list of ints separated by given separator
+func MultiIntValidatorFactory(separator string) Validator {
+	return func(input interface{}) (interface{}, error) {
+		switch reflect.TypeOf(input).Kind() {
+		case reflect.Array:
+			fallthrough
+		case reflect.Slice:
+			out := make([]int, 0)
+			slc := reflect.ValueOf(input)
+			validator := IntValidatorFactory()
+			for i := 0; i < slc.Len(); i++ {
+				itm, err := validator(slc.Index(i))
+				if err != nil {
+					return nil, fmt.Errorf("value (%v) is not list of integers", input)
+				}
+				out = append(out, itm.(int))
+			}
+			return out, nil
+		case reflect.String:
+			out := make([]int, 0)
+			for _, item := range strings.Split(input.(string), separator) {
+				itm, err := strconv.Atoi(item)
+				if err != nil {
+					return nil, err
+				}
+				out = append(out, itm)
+			}
+			return out, nil
+		default:
+			return nil, fmt.Errorf("value (%v) is not list of integers", input)
+		}
+	}
+}
+
+//TODO: Add more builtin validators
